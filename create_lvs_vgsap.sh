fbe8dd170395febeec2dfe3b79c21e47c9610ee5
Diogo Kersting <diogovk@gmail.com>
2014-04-03 15:27:25 -0300

    swap and vg_sap

--- create_lvs_vgsap.sh
+++ create_lvs_vgsap.sh
@@ -1,15 +1,18 @@
 
 set -e
 VG=vg_sap
-#pvcreate /dev/sdc
+pvcreate /dev/sdc
 vgcreate $VG /dev/sdc
 
 source utils.sh
 
-# create_ext4_lvm  /sapmnt       lv_sapmnt       1G
-# create_ext4_lvm  /sapmnt/POQ   lv_sapmnt_poq   10G
-# create_ext4_lvm  /usr/sap      lv_usr_sap      1G
-# create_ext4_lvm  /usr/sap/DAA  lv_usr_sap_daa  5G
-# create_ext4_lvm  /usr/sap/POQ  lv_usr_sap_poq  10G
+create_ext4_lvm  /sapmnt         lv_sapmnt       1G
+create_ext4_lvm  /sapmnt/JBD     lv_sapmnt_jbd   10G
+create_ext4_lvm  /sapmnt/BWD     lv_sapmnt_bwd   10G
+create_ext4_lvm  /usr/sap        lv_usr_sap      1G
+create_ext4_lvm  /usr/sap/DAA    lv_usr_sap_daa  5G
+create_ext4_lvm  /usr/sap/JBD    lv_usr_sap_jdb  10G
+create_ext4_lvm  /usr/sap/BWD    lv_usr_sap_bwd  10G
+create_ext4_lvm  /usr/sap/trans  lv_usr_trans    30G
 
 
--- create_swap.sh
+++ create_swap.sh
@@ -1,11 +1,11 @@
+# parted /dev/sdb mklabel msdos
+# parted /dev/sdb mkpart primary linux-swap 0 17.2GB Ignore
+set -e
 
-# parted
-# mkpart primary linux-swap 0 17.2GB
-
-#mkswap /dev/sdb1 
-#echo "UUID=97234cd9-5944-42cb-81c5-a0ea43f77718 none  swap    defaults        0 0" >> /etc/fstab
-#swapon -a
-#swapon -s
+mkswap /dev/sdb1 
+echo "/dev/sdb1               none                    swap    defaults        0 0" >> /etc/fstab
+swapon -a
+swapon -s
 
 
 
