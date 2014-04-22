73127acb412da43f5270b0afd0e17dd1bc87b35b
Diogo Kersting <diogovk@gmail.com>
2014-04-22 11:14:00 -0300

    swap

--- create_swap.sh
+++ create_swap.sh
@@ -1,6 +1,7 @@
-# parted /dev/sdb mklabel msdos
-# parted /dev/sdb mkpart primary linux-swap 0 17.2GB Ignore
 set -e
+
+parted /dev/sdb mklabel msdos
+parted /dev/sdb mkpart primary linux-swap 0 17.2GB Ignore
 
 mkswap /dev/sdb1 
 echo "/dev/sdb1               none                    swap    defaults        0 0" >> /etc/fstab
