cac832b767a8b4dcc58434a9efe4b55fdf36ddcf
Diogo Kersting <diogovk@gmail.com>
2014-04-22 10:21:05 -0300

    kernel parameters

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,8 @@
 
-echo kernel.sem=1250 256000 100 1024 >> /etc/sysctl.conf
-echo vm.max_map_count=2000000 >> /etc/sysctl.conf
+echo '# SAP settings
+kernel.msgmni=1024
+kernel.sem=1250 256000 100 1024
+vm.max_map_count=2000000' >> /etc/sysctl.conf
 
 sysctl -p
 
