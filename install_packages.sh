cd726aca0df53a1d46340fb55040489e071986d1
Diogo Kersting <diogovk@gmail.com>
2014-08-22 16:21:18 -0300

    script for internal repo configuration

--- install_packages.sh
+++ install_packages.sh
@@ -2,5 +2,5 @@ subscription-manager register
 
 subscription-manager attach --pool 8a85f98244800ff201451ee9a392212b
 
-yum -y install uuidd ksh
+yum -y install uuidd ksh kernel-devel
 
---
+++ internal_repo_config.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yum-config-manager --disable rhel-6-server-cf-tools-1-rpms
+yum-config-manager --disable rhel-6-server-rpms
+
+cat > /etc/yum.repos.d/srvspsld01.repo  << \EOF
+[rhel-local]
+gpgcheck=0
+name=Red Hat Linux $releasever - $basearch - For Malwee
+baseurl=http://srvspsld01/
+EOF
+
+sed -i s/keepcache=0/keepcache=1/ /etc/yum.conf 
+yum clean all
