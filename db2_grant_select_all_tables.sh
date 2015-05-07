1bf00d2f50c9edd66ac5748d7653d91edd587514
Diogo Kersting <diogovk@gmail.com>
2015-05-07 10:45:51 -0300

    Show how to create role and assing to user

--- db2_grant_select_all_tables.sh
+++ db2_grant_select_all_tables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## Create role:
+# db2 create role sapreadonly
+## Grant role to user
+# db2 grant sapreadonly to user my_user
+
 # Gives select permission to all tables in schema SAPECD to the role sapreadonly
 db2 -tnx "select distinct 'GRANT select ON TABLE '||
        '\"'||rtrim(tabschema)||'\".\"'||rtrim(tabname)||'\" TO role sapreadonly;'
